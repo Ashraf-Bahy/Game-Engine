b11dc27d7b19bb3de78f47aacebb8ec1dcf68b3a
Ashraf-Bahy <ashrafbahy286@gmail.com>
2025-04-25 20:05:19 +0300

    Loading gltf models support done

--- source/common/mesh/mesh-utils.cpp
+++ source/common/mesh/mesh-utils.cpp
@@ -3,12 +3,14 @@
 // We will use "Tiny OBJ Loader" to read and process '.obj" files
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tinyobj/tiny_obj_loader.h>
+#include <tiny_gltf.h>
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 
-our::Mesh* our::mesh_utils::loadOBJ(const std::string& filename) {
+our::Mesh *our::mesh_utils::loadOBJ(const std::string &filename)
+{
 
     // The data that we will use to initialize our mesh
     std::vector<our::Vertex> vertices;
@@ -25,56 +27,58 @@ our::Mesh* our::mesh_utils::loadOBJ(const std::string& filename) {
     std::vector<tinyobj::material_t> materials;
     std::string warn, err;
 
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str())) {
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str()))
+    {
         std::cerr << "Failed to load obj file \"" << filename << "\" due to error: " << err << std::endl;
         return nullptr;
     }
-    if (!warn.empty()) {
+    if (!warn.empty())
+    {
         std::cout << "WARN while loading obj file \"" << filename << "\": " << warn << std::endl;
     }
 
     // An obj file can have multiple shapes where each shape can have its own material
     // Ideally, we would load each shape into a separate mesh or store the start and end of it in the element buffer to be able to draw each shape separately
     // But we ignored this fact since we don't plan to use multiple materials in the examples
-    for (const auto &shape : shapes) {
-        for (const auto &index : shape.mesh.indices) {
+    for (const auto &shape : shapes)
+    {
+        for (const auto &index : shape.mesh.indices)
+        {
             Vertex vertex = {};
 
             // Read the data for a vertex from the "attrib" object
             vertex.position = {
-                    attrib.vertices[3 * index.vertex_index + 0],
-                    attrib.vertices[3 * index.vertex_index + 1],
-                    attrib.vertices[3 * index.vertex_index + 2]
-            };
+                attrib.vertices[3 * index.vertex_index + 0],
+                attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2]};
 
             vertex.normal = {
-                    attrib.normals[3 * index.normal_index + 0],
-                    attrib.normals[3 * index.normal_index + 1],
-                    attrib.normals[3 * index.normal_index + 2]
-            };
+                attrib.normals[3 * index.normal_index + 0],
+                attrib.normals[3 * index.normal_index + 1],
+                attrib.normals[3 * index.normal_index + 2]};
 
             vertex.tex_coord = {
-                    attrib.texcoords[2 * index.texcoord_index + 0],
-                    attrib.texcoords[2 * index.texcoord_index + 1]
-            };
-
+                attrib.texcoords[2 * index.texcoord_index + 0],
+                1.0f - attrib.texcoords[2 * index.texcoord_index + 1]};
 
             vertex.color = {
-                    attrib.colors[3 * index.vertex_index + 0] * 255,
-                    attrib.colors[3 * index.vertex_index + 1] * 255,
-                    attrib.colors[3 * index.vertex_index + 2] * 255,
-                    255
-            };
+                attrib.colors[3 * index.vertex_index + 0] * 255,
+                attrib.colors[3 * index.vertex_index + 1] * 255,
+                attrib.colors[3 * index.vertex_index + 2] * 255,
+                255};
 
             // See if we already stored a similar vertex
             auto it = vertex_map.find(vertex);
-            if (it == vertex_map.end()) {
+            if (it == vertex_map.end())
+            {
                 // if no, add it to the vertices and record its index
                 auto new_vertex_index = static_cast<GLuint>(vertices.size());
                 vertex_map[vertex] = new_vertex_index;
                 elements.push_back(new_vertex_index);
                 vertices.push_back(vertex);
-            } else {
+            }
+            else
+            {
                 // if yes, just add its index in the elements vector
                 elements.push_back(it->second);
             }
@@ -84,19 +88,269 @@ our::Mesh* our::mesh_utils::loadOBJ(const std::string& filename) {
     return new our::Mesh(vertices, elements);
 }
 
+our::Mesh *our::mesh_utils::loadGLTF(const std::string &filename)
+{
+    // The data that we will use to initialize our mesh
+    std::vector<our::Vertex> vertices;
+    std::vector<GLuint> elements;
+
+    // Since we may have duplicated vertices, we can make them unique
+    std::unordered_map<our::Vertex, GLuint> vertex_map;
+
+    // Load GLTF model
+    tinygltf::Model model;
+    tinygltf::TinyGLTF loader;
+    std::string err;
+    std::string warn;
+
+    bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
+
+    if (!warn.empty())
+    {
+        std::cout << "WARN while loading gltf file \"" << filename << "\": " << warn << std::endl;
+    }
+
+    if (!err.empty())
+    {
+        std::cerr << "ERROR loading gltf file \"" << filename << "\": " << err << std::endl;
+        return nullptr;
+    }
+
+    if (!ret)
+    {
+        std::cerr << "Failed to load gltf file \"" << filename << "\"" << std::endl;
+        return nullptr;
+    }
+
+    // Process all meshes in the GLTF file (assuming we want to combine all meshes)
+    for (size_t meshIndex = 0; meshIndex < model.meshes.size(); ++meshIndex)
+    {
+        const tinygltf::Mesh &mesh = model.meshes[meshIndex];
+
+        // Process each primitive in the mesh
+        for (size_t primitiveIndex = 0; primitiveIndex < mesh.primitives.size(); ++primitiveIndex)
+        {
+            const tinygltf::Primitive &primitive = mesh.primitives[primitiveIndex];
+
+            // Get the accessor indices for the attributes we need
+            int positionAccessorIndex = primitive.attributes.find("POSITION") != primitive.attributes.end() ? primitive.attributes.at("POSITION") : -1;
+            int normalAccessorIndex = primitive.attributes.find("NORMAL") != primitive.attributes.end() ? primitive.attributes.at("NORMAL") : -1;
+            int texcoordAccessorIndex = primitive.attributes.find("TEXCOORD_0") != primitive.attributes.end() ? primitive.attributes.at("TEXCOORD_0") : -1;
+            int colorAccessorIndex = primitive.attributes.find("COLOR_0") != primitive.attributes.end() ? primitive.attributes.at("COLOR_0") : -1;
+
+            // Get the indices accessor
+            int indicesAccessorIndex = primitive.indices;
+
+            // Skip primitives with no positions
+            if (positionAccessorIndex == -1)
+                continue;
+
+            // Get accessors
+            const tinygltf::Accessor &positionAccessor = model.accessors[positionAccessorIndex];
+            const tinygltf::BufferView &positionBufferView = model.bufferViews[positionAccessor.bufferView];
+            const tinygltf::Buffer &positionBuffer = model.buffers[positionBufferView.buffer];
+
+            // Calculate position stride - use the bufferView stride if specified, otherwise calculate it
+            int positionStride = positionBufferView.byteStride ? positionBufferView.byteStride : (positionAccessor.type == TINYGLTF_TYPE_VEC3 ? 3 * sizeof(float) : 0);
+
+            // Get view for normal if it exists
+            const tinygltf::Accessor *normalAccessor = nullptr;
+            const tinygltf::BufferView *normalBufferView = nullptr;
+            const tinygltf::Buffer *normalBuffer = nullptr;
+            int normalStride = 0;
+            if (normalAccessorIndex != -1)
+            {
+                normalAccessor = &model.accessors[normalAccessorIndex];
+                normalBufferView = &model.bufferViews[normalAccessor->bufferView];
+                normalBuffer = &model.buffers[normalBufferView->buffer];
+                normalStride = normalBufferView->byteStride ? normalBufferView->byteStride : (normalAccessor->type == TINYGLTF_TYPE_VEC3 ? 3 * sizeof(float) : 0);
+            }
+
+            // Get view for texcoord if it exists
+            const tinygltf::Accessor *texcoordAccessor = nullptr;
+            const tinygltf::BufferView *texcoordBufferView = nullptr;
+            const tinygltf::Buffer *texcoordBuffer = nullptr;
+            int texcoordStride = 0;
+            if (texcoordAccessorIndex != -1)
+            {
+                texcoordAccessor = &model.accessors[texcoordAccessorIndex];
+                texcoordBufferView = &model.bufferViews[texcoordAccessor->bufferView];
+                texcoordBuffer = &model.buffers[texcoordBufferView->buffer];
+                texcoordStride = texcoordBufferView->byteStride ? texcoordBufferView->byteStride : (texcoordAccessor->type == TINYGLTF_TYPE_VEC2 ? 2 * sizeof(float) : 0);
+            }
+
+            // Get view for color if it exists
+            const tinygltf::Accessor *colorAccessor = nullptr;
+            const tinygltf::BufferView *colorBufferView = nullptr;
+            const tinygltf::Buffer *colorBuffer = nullptr;
+            int colorStride = 0;
+            if (colorAccessorIndex != -1)
+            {
+                colorAccessor = &model.accessors[colorAccessorIndex];
+                colorBufferView = &model.bufferViews[colorAccessor->bufferView];
+                colorBuffer = &model.buffers[colorBufferView->buffer];
+                colorStride = colorBufferView->byteStride ? colorBufferView->byteStride : (colorAccessor->type == TINYGLTF_TYPE_VEC3 ? 3 * sizeof(float) : (colorAccessor->type == TINYGLTF_TYPE_VEC4 ? 4 * sizeof(float) : 0));
+            }
+
+            // Get indices
+            const tinygltf::Accessor &indicesAccessor = model.accessors[indicesAccessorIndex];
+            const tinygltf::BufferView &indicesBufferView = model.bufferViews[indicesAccessor.bufferView];
+            const tinygltf::Buffer &indicesBuffer = model.buffers[indicesBufferView.buffer];
+
+            // Calculate vertex count
+            size_t vertexCount = positionAccessor.count;
+
+            // Create vertices
+            std::vector<our::Vertex> primitiveVertices;
+            primitiveVertices.reserve(vertexCount);
+
+            for (size_t vertexIndex = 0; vertexIndex < vertexCount; ++vertexIndex)
+            {
+                our::Vertex vertex = {};
+
+                // Position (required)
+                const float *positions = reinterpret_cast<const float *>(&positionBuffer.data[positionBufferView.byteOffset + positionAccessor.byteOffset +
+                                                                                              (positionStride ? vertexIndex * positionStride : vertexIndex * 3 * sizeof(float))]);
+                vertex.position = {positions[0], positions[1], positions[2]};
+
+                // Normal (optional)
+                if (normalAccessor)
+                {
+                    const float *normals = reinterpret_cast<const float *>(&normalBuffer->data[normalBufferView->byteOffset + normalAccessor->byteOffset +
+                                                                                               (normalStride ? vertexIndex * normalStride : vertexIndex * 3 * sizeof(float))]);
+                    vertex.normal = {normals[0], normals[1], normals[2]};
+                }
+                else
+                {
+                    vertex.normal = {0.0f, 0.0f, 1.0f}; // Default normal
+                }
+
+                // Texture coordinates (optional)
+                if (texcoordAccessor)
+                {
+                    const float *texcoords = reinterpret_cast<const float *>(&texcoordBuffer->data[texcoordBufferView->byteOffset + texcoordAccessor->byteOffset +
+                                                                                                   (texcoordStride ? vertexIndex * texcoordStride : vertexIndex * 2 * sizeof(float))]);
+
+                    // OpenGL expects texture coordinates with origin at bottom-left
+                    // GLTF has origin at top-left, so we flip Y coordinate
+                    vertex.tex_coord = {texcoords[0], 1.0f - texcoords[1]};
+                }
+                else
+                {
+                    vertex.tex_coord = {0.0f, 0.0f}; // Default UV
+                }
+
+                // Color (optional)
+                if (colorAccessor)
+                {
+                    if (colorAccessor->type == TINYGLTF_TYPE_VEC3)
+                    {
+                        // RGB color
+                        const float *colors = reinterpret_cast<const float *>(&colorBuffer->data[colorBufferView->byteOffset + colorAccessor->byteOffset +
+                                                                                                 (colorStride ? vertexIndex * colorStride : vertexIndex * 3 * sizeof(float))]);
+                        vertex.color = {
+                            static_cast<uint8_t>(colors[0] * 255),
+                            static_cast<uint8_t>(colors[1] * 255),
+                            static_cast<uint8_t>(colors[2] * 255),
+                            255};
+                    }
+                    else if (colorAccessor->type == TINYGLTF_TYPE_VEC4)
+                    {
+                        // RGBA color
+                        const float *colors = reinterpret_cast<const float *>(&colorBuffer->data[colorBufferView->byteOffset + colorAccessor->byteOffset +
+                                                                                                 (colorStride ? vertexIndex * colorStride : vertexIndex * 4 * sizeof(float))]);
+                        vertex.color = {
+                            static_cast<uint8_t>(colors[0] * 255),
+                            static_cast<uint8_t>(colors[1] * 255),
+                            static_cast<uint8_t>(colors[2] * 255),
+                            static_cast<uint8_t>(colors[3] * 255)};
+                    }
+                }
+                else
+                {
+                    vertex.color = {255, 255, 255, 255}; // Default color
+                }
+
+                // Add vertex to the primitive vertices
+                primitiveVertices.push_back(vertex);
+            }
+
+            // Process indices and add to our elements list
+            size_t indexCount = indicesAccessor.count;
+            size_t baseVertex = vertices.size(); // Offset for the current primitive
+
+            for (size_t indexIndex = 0; indexIndex < indexCount; ++indexIndex)
+            {
+                uint32_t vertexIndex = 0;
+
+                if (indicesAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT)
+                {
+                    vertexIndex = *reinterpret_cast<const uint16_t *>(&indicesBuffer.data[indicesBufferView.byteOffset + indicesAccessor.byteOffset +
+                                                                                          indexIndex * sizeof(uint16_t)]);
+                }
+                else if (indicesAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT)
+                {
+                    vertexIndex = *reinterpret_cast<const uint32_t *>(&indicesBuffer.data[indicesBufferView.byteOffset + indicesAccessor.byteOffset +
+                                                                                          indexIndex * sizeof(uint32_t)]);
+                }
+                else if (indicesAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE)
+                {
+                    vertexIndex = *reinterpret_cast<const uint8_t *>(&indicesBuffer.data[indicesBufferView.byteOffset + indicesAccessor.byteOffset +
+                                                                                         indexIndex * sizeof(uint8_t)]);
+                }
+                else
+                {
+                    continue; // Unsupported component type
+                }
+
+                // Get the Vertex from our primitive vertices
+                const our::Vertex &primitiveVertex = primitiveVertices[vertexIndex];
+
+                // Check if we've already added this vertex
+                auto it = vertex_map.find(primitiveVertex);
+                if (it == vertex_map.end())
+                {
+                    // If not, add it to our global vertex list
+                    GLuint newIndex = static_cast<GLuint>(vertices.size());
+                    vertex_map[primitiveVertex] = newIndex;
+                    vertices.push_back(primitiveVertex);
+                    elements.push_back(newIndex);
+                }
+                else
+                {
+                    // If yes, just use its index
+                    elements.push_back(it->second);
+                }
+            }
+        }
+    }
+
+    // Create and return the mesh
+    if (vertices.empty() || elements.empty())
+    {
+        std::cerr << "No valid mesh data found in GLTF file \"" << filename << "\"" << std::endl;
+        return nullptr;
+    }
+
+    return new our::Mesh(vertices, elements);
+}
+
 // Create a sphere (the vertex order in the triangles are CCW from the outside)
 // Segments define the number of divisions on the both the latitude and the longitude
-our::Mesh* our::mesh_utils::sphere(const glm::ivec2& segments){
+our::Mesh *our::mesh_utils::sphere(const glm::ivec2 &segments)
+{
     std::vector<our::Vertex> vertices;
     std::vector<GLuint> elements;
 
     // We populate the sphere vertices by looping over its longitude and latitude
-    for(int lat = 0; lat <= segments.y; lat++){
+    for (int lat = 0; lat <= segments.y; lat++)
+    {
         float v = (float)lat / segments.y;
         float pitch = v * glm::pi<float>() - glm::half_pi<float>();
         float cos = glm::cos(pitch), sin = glm::sin(pitch);
-        for(int lng = 0; lng <= segments.x; lng++){
-            float u = (float)lng/segments.x;
+        for (int lng = 0; lng <= segments.x; lng++)
+        {
+            float u = (float)lng / segments.x;
             float yaw = u * glm::two_pi<float>();
             glm::vec3 normal = {cos * glm::cos(yaw), sin, cos * glm::sin(yaw)};
             glm::vec3 position = normal;
@@ -106,10 +360,12 @@ our::Mesh* our::mesh_utils::sphere(const glm::ivec2& segments){
         }
     }
 
-    for(int lat = 1; lat <= segments.y; lat++){
-        int start = lat*(segments.x+1);
-        for(int lng = 1; lng <= segments.x; lng++){
-            int prev_lng = lng-1;
+    for (int lat = 1; lat <= segments.y; lat++)
+    {
+        int start = lat * (segments.x + 1);
+        for (int lng = 1; lng <= segments.x; lng++)
+        {
+            int prev_lng = lng - 1;
             elements.push_back(lng + start);
             elements.push_back(lng + start - segments.x - 1);
             elements.push_back(prev_lng + start - segments.x - 1);
